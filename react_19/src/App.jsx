8fc817edde122843682d0dc09ea0ec47528427d2
Amruth <ammumandappa@gmail.com>
2025-02-09 12:37:12 +0530

    .

--- react_19/src/App.jsx
+++ react_19/src/App.jsx
@@ -1,15 +1,23 @@
-import  UserProfile  from "./components/UserProfile";
+import UserProfile from "./components/UserProfile";
 
-function App(){
-    const callMe = () => {
-        console.log("hello")
-    }
-    return (
-        <div>
-            Root Component 
-            <UserProfile username = "App.js" age = {20} favoriteFoods={[{name: 'sushi'}, {name: "biriyani"}]} callMe= {callMe}></UserProfile>
-        </div>
-    )
+function App() {
+  const callMe = () => {
+    console.log("hello");
+  };
+  return (
+    <div>
+      Root Component
+      <UserProfile
+        username="App.js"
+        age={20}
+        favoriteFoods={[
+          { name: "Sushi", id: 1 },
+          { name: "Biriyani", id: 2 },
+        ]}
+        callMe={callMe}
+      ></UserProfile>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- react_19/src/components/UserProfile.jsx
+++ react_19/src/components/UserProfile.jsx
@@ -1,35 +1,42 @@
-import {UserFavoriteFoods} from './UserFavoriteFoods';
-import { UserUsername } from './USerUsername';
-import PropTypes from 'prop-types'
+import { UserFavoriteFoods } from "./UserFavoriteFoods";
+import { UserUsername } from "./USerUsername";
+import PropTypes from "prop-types";
 
-function UserProfile(props){
-    console.log(props)
-    return (
-        <div>
-            <UserUsername username={props.username}></UserUsername>
-            <div>
-                <span>Email: </span> 
-                <span>bob@gmail.com</span>
-                <br />
-                <b>Age: </b><span>{props.age}</span>
-            </div>
-        
-           
-            <UserFavoriteFoods></UserFavoriteFoods>
-            <br />
-        {props.favoriteFoods.map((food, index) => (
-            <div key={index}>
-                <b>Favorite Food {index + 1}: </b><span>{food.name}</span>
-            </div>
-        ))}
+function UserProfile(props) {
+  console.log(props);
+  return (
+    <div>
+      <UserUsername username={props.username}></UserUsername>
+      <div>
+        <span>Email: </span>
+        <span>bob@gmail.com</span>
+        <br />
+        <b>Age: </b>
+        <span>{props.age}</span>
+      </div>
+
+      <UserFavoriteFoods></UserFavoriteFoods>
+      <br />
+      {props.favoriteFoods.map((food, index) => (
+        <div key={index}>
+          <b>Favorite Food {index + 1}: </b>
+          <span>{food.name}</span>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-UserProfile.protoTypes = {
-    username: PropTypes.string.isRequired,
-    age: PropTypes.number.isRequired,
-    favoriteFoods: PropTypes.array.isRequired
-}
+UserProfile.propTypes = {
+  username: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  favoriteFoods: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+};
 
 export default UserProfile;
