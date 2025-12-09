74d6e6852b08fddc9c1e71903686d423d338f681
Amruth <ammumandappa@gmail.com>
2025-12-09 22:28:19 +0530

    created new react query project

--- formHook/src/App.jsx
+++ formHook/src/App.jsx
@@ -1,26 +1,56 @@
-import { useState } from 'react'
-import { useForm} from 'react-hook-form'
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function App() {
-const {register, handleSubmit} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-const onSubmit = (data) => {
-  console.log(data);
-}
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
-  <form className='gap-2' onSubmit={handleSubmit(onSubmit)}>
-    <input {...register("emAIL", {
-      required: true,
-      pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-    })} type="text" placeholder="Email"></input>
-    <input {...register("password", {
-      required: true,
-      minLength: 6,
-    })}type="password" placeholder ="password"></input>
-    <button type = "submit">Submit</button>
-  </form>
-  )
+    <form className="gap-2" onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register("emAIL", {
+          required: "Email is required",
+          // pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+          validate: (value) => {
+            if(!value.includes("@")){
+              return "Email must contain @ symbol";
+            }
+            return true;
+          }
+        })}
+        type="text"
+        placeholder="Email"
+      ></input>
+      {
+        errors.emAIL && (
+          <div>
+            {errors.emAIL.message}
+          </div>
+        )
+      }
+      <input
+        {...register("password", {
+          required: "Password is required",
+          minLength: 6,
+        })}
+        type="password"
+        placeholder="password"
+      ></input>
+      {errors.password && (
+        <div>
+          {errors.password.message}
+        </div>
+      )}
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
-export default App
+export default App;
