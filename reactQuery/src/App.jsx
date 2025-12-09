ab92df6427097186da05576a8f1863ffeb04b68b
Amruth <ammumandappa@gmail.com>
2025-12-09 23:01:49 +0530

    created routes

--- reactQuery/src/App.jsx
+++ reactQuery/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import MainLayout from './components/Layout/MainLayout';
+import Home from './Pages/Home';
+import FetchOld from './Pages/FetchOld';
+import FetchRq from './Pages/FetchRq';
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout/>,
+    children: [
+      {
+        path:"/",
+        element: <Home/>,
+      },
+      {
+        path: "/trad",
+        element: <FetchOld/>,
+      },
+      {
+        path: "/rq",
+        element: <FetchRq/>,
+      },
+    ],
+  },
+]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+ return <RouterProvider router={router}></RouterProvider>
 }
 
-export default App
+export default App;
