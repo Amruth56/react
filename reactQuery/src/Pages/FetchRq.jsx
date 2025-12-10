aaa6908019e984f83bce9a654909efe7aa2d4ba5
Amruth <ammumandappa@gmail.com>
2025-12-10 09:47:24 +0530

    used reactQuery tio fetch the data

--- reactQuery/src/Pages/FetchRq.jsx
+++ reactQuery/src/Pages/FetchRq.jsx
@@ -1,7 +1,39 @@
+import { fetchPosts } from "../API/api";
+import {useQuery} from '@tanstack/react-query'
 
 const FetchRq = () => {
+
+  const getPostsData = async() => {
+    try{
+      const res = await fetchPosts();
+      const data = res.data;
+      console.log(data)
+
+     return res.status === 200 ? data: [];
+
+    }catch(err){
+      console.log(err);
+      return [];
+    }
+  }
+
+  const {data} = useQuery({
+    queryKey:['posts'],
+    queryFn: getPostsData,
+  })
+
   return (
-    <div>FetchRq</div>
+    <div>
+      {data?.map((curEle) => 
+      {
+        return (
+          <li key={curEle.id}>
+            <h3>{curEle.title}</h3>
+            <p>{curEle.body}</p>
+          </li>
+        )
+      })}
+    </div>
   )
 }
 
