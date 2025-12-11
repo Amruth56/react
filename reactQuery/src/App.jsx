2d6e2878e543249fab322bd0e1bcfdc8778439aa
Amruth <ammumandappa@gmail.com>
2025-12-11 17:57:26 +0530

    created infinite scroll page

--- reactQuery/src/App.jsx
+++ reactQuery/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Pages/Home';
 import FetchOld from './Pages/FetchOld';
 import FetchRq from './Pages/FetchRq';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import InfiniteScroll from './Pages/InfiniteScroll';
 
 const router = createBrowserRouter([
   {
@@ -22,12 +23,16 @@ const router = createBrowserRouter([
         path: "/rq",
         element: <FetchRq/>,
       },
+      {
+        path:'/scroll',
+        element:<InfiniteScroll/>
+      }
     ],
   },
 ]);
 
 const App = () => {
-  const queryClient = new QueryClient
+  const queryClient = new QueryClient()
  return(
  <QueryClientProvider client={queryClient}>
  <RouterProvider router={router}></RouterProvider>
